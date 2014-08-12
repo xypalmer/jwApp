8f5165d8bfaf61e35cd486de5927e9da6a3ed72f
xypalmer <xypalmer@icloud.com>
2014-08-12 12:02:18 -0700

    contenteditable now actually working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,25 +3,21 @@ var jwApp = angular.module('jwApp', []);
 
 jwApp.controller('jwController', function($scope) {
 
-console.log('hello');
-
-// $scope.myFunction = function () {
-// document.getElementById('writeHere').contentWindow.document.designMode="on";
-// console.log('hello2');
-// };
-
 $scope.boldText = function ()
     {  
-       var boldIt = document.getElementById("writeHere");
-       boldIt.document.execCommand("bold", false, ""); 
-       console.log("bold");
+        document.execCommand("bold", false, ""); 
+        console.log("bold");
     }
 
 $scope.italicizeText = function ()
      {  
-        var italicizeIt = document.getElementById("writeHere");
-        italicizeIt.document.execCommand("italic", false, ""); 
-        console.log("bold");
+        document.execCommand("italic", false, ""); 
+        console.log("italicize");
+     }
+$scope.underlineText = function ()
+     {  
+        document.execCommand("underline", false, ""); 
+        console.log("underline");
      }
 $scope.submit_form = function () {
   document.getElementById("project_text").value = document.getElementById('writeHere').contentWindow.document.body.innerHTML;
