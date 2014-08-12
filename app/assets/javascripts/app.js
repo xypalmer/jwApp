ce3aed14ac06f06c5bd7cd9b7210039aa89d9179
xypalmer <xypalmer@icloud.com>
2014-08-12 11:36:48 -0700

    contenteditable working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,21 +5,21 @@ jwApp.controller('jwController', function($scope) {
 
 console.log('hello');
 
-$scope.myFunction = function () {
-document.getElementById('writeHere').contentWindow.document.designMode="on";
-console.log('hello2');
-};
+// $scope.myFunction = function () {
+// document.getElementById('writeHere').contentWindow.document.designMode="on";
+// console.log('hello2');
+// };
 
 $scope.boldText = function ()
     {  
-       var boldIt = document.getElementById("writeHere").contentWindow;
+       var boldIt = document.getElementById("writeHere");
        boldIt.document.execCommand("bold", false, ""); 
        console.log("bold");
     }
 
 $scope.italicizeText = function ()
      {  
-        var italicizeIt = document.getElementById("writeHere").contentWindow;
+        var italicizeIt = document.getElementById("writeHere");
         italicizeIt.document.execCommand("italic", false, ""); 
         console.log("bold");
      }
@@ -31,11 +31,33 @@ $scope.submit_form2 = function () {
   document.getElementById('writeHere').contentWindow.document.body.innerHTML = document.getElementById("project_text").value;
 }
 
-$scope.iframeW = document.getElementById('writeHere').contentWindow.document.body.innerHTML;
+// $scope.iframeW = document.getElementById('writeHere').contentWindow.document.body.innerHTML;
 
-$scope.$watch('iframeW', function (newValue, oldValue) {
-  $scope.submit_form();
-  console.log('changed');
-}, true);
+// $scope.$watch('iframeW', function (newValue, oldValue) {
+//   $scope.submit_form();
+//   console.log('changed');
+// }, true);
 
 })
+
+
+jwApp.directive("hello", function() {
+    return {
+      restrict: "A",
+      require: "ngModel",
+      link: function(scope, element, attrs, ngModel) {
+
+        function read() {
+          ngModel.$setViewValue(element.html());
+        }
+
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue || "");
+        };
+
+        element.bind("blur keyup change", function() {
+          scope.$apply(read);
+        });
+      }
+    };
+  });
