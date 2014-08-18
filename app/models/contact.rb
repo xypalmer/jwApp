15ff6edc041294cf5af18bd2cc0e9a237af5dd84
xypalmer <xypalmer@icloud.com>
2014-08-17 18:22:18 -0700

    rich text email working

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
   attribute :title,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :email
   attribute :message
   attribute :nickname,  :captcha  => true
 
